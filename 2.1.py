e257e8c74ac7c855d39422b58f2d9e829b7ab9e4
camiromo3 <camiloromo95@gmail.com>
2024-06-29 13:42:53 -0500

    subir

---
+++ 2.1.py
@@ -0,0 +1,6 @@
+print("estes es un proyecto de operaciones matematicas que verifica la iguladad de dos numeros")
+nombre = input("por favor ingresar nombre ")
+num1 = int(input(f"{nombre} ingrese el primer numero"))
+num2 = int(input(f"{nombre} ingrese el segundo numero"))
+igualdad = num1==num2
+print(f"{nombre} los numeros {num1} y {num2} son {igualdad}")
