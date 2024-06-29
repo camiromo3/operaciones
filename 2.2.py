1975a992c15ec55c792a0c3b8647bf81eed77b59
camiromo3 <camiloromo95@gmail.com>
2024-06-29 13:41:41 -0500

    este es otro commit

---
+++ 2.2.py
@@ -0,0 +1,7 @@
+print("estes es un proyecto de operaciones matematicas verifica el desigual de dos numeros")
+nombre = input("por favor ingresar nombre ")
+num1 = int(input(f"{nombre} ingrese el primer numero"))
+num2 = int(input(f"{nombre} ingrese el segundo numero"))
+desigual = num1 != num2
+print(f"{nombre} los numeros {num1} y {num2} son {desigual}")
+
---
+++ 2.5.py
@@ -0,0 +1,6 @@
+print("estes es un proyecto de operaciones matematicas verifica si un numero x es mayor o igual que un numero y")
+nombre = input("por favor ingresar nombre ")
+num1 = int(input(f"{nombre} ingrese el numero x"))
+num2 = int(input(f"{nombre} ingrese el numero y"))
+mayorigual = num1 >= num2
+print(f"{nombre} los numeros {num1} y {num2} son {mayorigual}")
